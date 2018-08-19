8c06a2b5507d3d9d48dadc3b695be0c0ecd9f95a
gbs0 <gabriel@lewagon.org>
2018-08-19 19:30:09 -0300

    Initializing js file

---
+++ grid_vanila/scripts.js
@@ -0,0 +1,12 @@
+const cards = document.querySelectorAll('.memory-card');
+
+function flipCard() {
+	this.classList.toggle('flip');
+}
+
+// Operations in array
+// cards.for (var i = 0; i < Things.length; i++) {
+// 	Things[i]
+// }
+
+cards.forEach(card => card.addEventListener('click', flipCard));
