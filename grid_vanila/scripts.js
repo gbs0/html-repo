c89af3962ff7bf93d28583701715be3c69241be9
gbs0 <gabriel@lewagon.org>
2018-08-20 08:32:14 -0300

    Toggle switch for cards element

--- grid_vanila/scripts.js
+++ grid_vanila/scripts.js
@@ -1,7 +1,21 @@
 const cards = document.querySelectorAll('.element-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
 	this.classList.toggle('flip');
+} else {
+	// First Click
+	hasFlippedCard = true;
+	firstCard = this;
+} else {
+	// second click
+	hasFlippedCard = false;
+	secondCard = this;
+
+	console.log(firstCard.dataset.framework);
+	console.log(secondCard.dataset.framework);
 }
 
 // Operations in array
