959e54863b1ae18a00d8d4532fae216a88969289
gbs0 <gabriel@lewagon.org>
2018-08-20 15:34:21 -0300

    Fixed js import in html file

--- grid_vanila/scripts.js
+++ grid_vanila/scripts.js
@@ -4,21 +4,20 @@ let hasFlippedCard = false;
 let firstCard, secondCard;
 
 function flipCard() {
-	if (this == firstCard) return;
+	if (this === firstCard) return;
 
 	this.classList.add('flip');
-} else {
-	// First Click
-	hasFlippedCard = true;
-	firstCard = this;
-} else {
-	// second click
-	hasFlippedCard = false;
-	secondCard = this;
 
-	console.log(firstCard.dataset.framework);
-	console.log(secondCard.dataset.framework);
-}
+	if (!hasFlippedCard) {
+		hasFlippedCard = true;
+		firstCard = this;
+
+		return;
+	}
+
+	//second click
+	secondCard = this;
+} 
 
 // Operations in array
 // cards.for (var i = 0; i < Things.length; i++) {
