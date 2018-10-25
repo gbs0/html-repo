551f9dc55407f8b5aaa2b439c9dffa1d0ae65239
Gabriel Schiavo <gabriel@lewagon.org>
2018-10-25 16:02:22 -0200

    Finished Geocoder

--- Geocoder_search/lib/app.js
+++ Geocoder_search/lib/app.js
@@ -1,18 +1,36 @@
 // TODO: Put your JS code in here
-const api_key = "AIzaSyAJmoXsLZpDoVYaHxmZ0XZES8yHiC4ycO0";
+const apiKey = "AIzaSyAJmoXsLZpDoVYaHxmZ0XZES8yHiC4ycO0";
 
-const search_box = document.getElementById("form");
+const searchBox = document.getElementById("form");
 
-search_box.addEventListener("submit", (event) => {
+const google = window.google;
+
+function myMap(lat, lng) {
+  const mapCanvas = document.getElementById("map");
+  const myCenter = new google.maps.LatLng(lat, lng);
+  const mapOptions = { center: myCenter, zoom: 15 };
+  const map = new google.maps.Map(mapCanvas, mapOptions);
+  const marker = new google.maps.Marker({
+    position: myCenter,
+    animation: google.maps.Animation.BOUNCE });
+  marker.setMap(map);
+}
+
+
+searchBox.addEventListener("submit", (event) => {
   event.preventDefault();
   const input = document.getElementById("search").value;
   console.log(input);
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${input}&key=${api_key}`;
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${input}&key=${apiKey}`;
   fetch(url)
     .then(response => response.json())
     .then((data) => {
-      console.log(data.results[0].formatted_address);
-      console.log(data.results[0].geometry.location.lat);
-      console.log(data.results[0].geometry.location.lng);
-  });
-})
+      const formatedAddress = data.results[0].formatted_address;
+      // console.log(data.results[0].geometry.location.lat);
+      // console.log(data.results[0].geometry.location.lng);
+      const latitude = data.results[0].geometry.location.lat;
+      const longitude = data.results[0].geometry.location.lng;
+      myMap(latitude, longitude);
+    });
+});
+
