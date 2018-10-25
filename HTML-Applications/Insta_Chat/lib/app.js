096790f7aa088c21e973a0261dcc1d3da8bab374
Gabriel Schiavo <gabriel@lewagon.org>
2018-10-25 17:46:59 -0200

    Add insta chat app for DOM demo purposes

---
+++ HTML-Applications/Insta_Chat/lib/app.js
@@ -0,0 +1,16 @@
+const batch = 182; // change to your own batch id
+const baseUrl = "https://wagon-chat.herokuapp.com/";
+const messageBox = document.getElementById("comment-form");
+console.log(messageBox);
+// Your turn to code!
+
+messageBox.addEventListener("submit", (event) => {
+  event.preventDefault();
+  //Pick the input from user
+  const userMessage = document.getElementById("your-message").value;
+  console.log(userMessage);
+  const userName = document.getElementById("your-name").value;
+  console.log(userName);
+  // Add a messagem in the HMTL ul
+  message.insertAdjacentHTML("beforeend", `<li>${userMessage}(posted <span class="date">10 minutes ago</span>) by ${userName}</li>`);
+});
